12386fd0fb4b451bc7d26ad06786ea6fc3639bdb
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-07-07 22:36:29 +0700

    Adding Card and css for the form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,23 @@
 import React from "react";
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
 
 function AddUser() {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
+  //Card cannot be CSS -> className like props to Card.js
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input type="text" placeholder="Type your name here..." />
-      <label htmlFor="age">Age</label>
-      <input type="number" placeholder="Type your age here..." />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" placeholder="Type your name here..." />
+        <label htmlFor="age">Age</label>
+        <input type="number" placeholder="Type your age here..." />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 }
 
