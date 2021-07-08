8ba91f64fb5210f547f0406e6cbcaac8940f210b
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-07-08 12:13:18 +0700

    set some conditions for name and age

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,6 +9,9 @@ function AddUser() {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0)
+      return;
+    if (+enteredAge < 1) return; //+enteredAge is cast age to number for safe
     setEnteredUsername("");
     setEnteredAge("");
   };
